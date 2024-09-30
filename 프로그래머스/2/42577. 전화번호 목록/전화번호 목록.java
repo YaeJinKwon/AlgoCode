b2f9550a9af7cccc8220e7eb007cc078b3a43cb5
Grace <93317276+YaeJinKwon@users.noreply.github.com>
2024-09-30 10:44:16 +0900

    [level 2] Title: 전화번호 목록, Time: 268.17 ms, Memory: 223 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -3,16 +3,17 @@ import java.io.*;
 
 
 class Solution {
-     public boolean solution(String[] phoneBook) {
-        // 1. phoneBook을 sorting한다.
-        Arrays.sort(phoneBook);
+      public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        Map<String, Integer> map = new HashMap<>();
+        for(int i = 0; i < phone_book.length; i++){
+            map.put(phone_book[i], i);
+        }
+        for(int i = 0; i< phone_book.length; i++)
+            for(int j = 0; j <phone_book[i].length(); j++){
+                if(map.containsKey(phone_book[i].substring(0,j))) return false;
+            }
 
-        // 2. 1중 Loop을 돌며 앞 번호가 뒷 번호의 접두어인지 확인한다.
-        for (int i = 0; i < phoneBook.length - 1; i++)
-            if (phoneBook[i + 1].startsWith(phoneBook[i]))
-                return false;
-        
-        // 3. 여기까지 오면 접두어가 없다는 것이다.
-        return true;
+        return answer;
     }
 }
