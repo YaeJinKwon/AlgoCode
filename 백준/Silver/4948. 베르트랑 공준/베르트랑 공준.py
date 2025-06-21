6de1949472ebc5af0360de5aff3099239431faa8
Grace <93317276+YaeJinKwon@users.noreply.github.com>
2025-06-21 22:52:32 +0900

    [Silver II] Title: 베르트랑 공준, Time: 120 ms, Memory: 35824 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
@@ -1,26 +1,21 @@
 import sys
-import math
 input = sys.stdin.readline
 
+MAX = 123456 * 2 + 1
+is_prime = [True] * MAX
+is_prime[0] = is_prime[1] = False
 
-while(True):
+# 에라토스테네스의 체로 소수 미리 구하기
+for i in range(2, int(MAX ** 0.5) + 1):
+    if is_prime[i]:
+        for j in range(i*i, MAX, i):
+            is_prime[j] = False
+
+while True:
     n = int(input())
-    if(n == 0):
+    if n == 0:
         break
-    ans = 0
-    for i in range(n+1, (2*n)+1):
-        c = 0
-        if(i<2):
-            continue
-        if(i==2):
-            ans+=1
-            continue
-        if(i%2==0):
-            continue
-        for k in range(2, math.isqrt(i)+1):
-            if i%k == 0: 
-                c=1
-                break
-        if (c == 0) : ans +=1
-    print(ans)
+    # n+1부터 2n까지 소수 개수 세기
+    print(sum(is_prime[n+1:2*n+1]))
 
+# 13 17 19 23
