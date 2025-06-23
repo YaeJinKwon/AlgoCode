77443f4847862c38a041d1cfb39249dddb379198
Grace <93317276+YaeJinKwon@users.noreply.github.com>
2025-06-24 06:27:59 +0900

    [Silver I] Title: 포도주 시식, Time: 40 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.py
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -8,26 +8,19 @@ data.append(0)
 for _ in range(n):
     data.append(int(input()))
 
-if(n==1):
+
+if n == 1:
     print(data[1])
-elif(n==2):
-    print(data[1]+data[2])
-elif(n==3):
-    dp = [0] *(n+1)
-    dp[0] = 0
-    dp[1] = data[1]
-    dp[2] = data[1]+data[2]
-    dp[3] = max(data[2]+dp[0], dp[1])+data[3]
-    print(max(dp))
-else:
-    dp = [0] *(n+1)
-    dp[0] = 0
-    dp[1] = data[1]
-    dp[2] = data[1]+data[2]
-    dp[3] = max(data[2]+dp[0], dp[1])+data[3]
-    for i in range(4,n+1):
-        dp[i] = max(data[i-1]+dp[i-3],dp[i-2] ,dp[i-4]+data[i-1])+data[i]
+    exit()
+
+dp = [0] * (n + 1)
+dp[1] = data[1]
+if n >= 2:
+    dp[2] = data[1] + data[2]
+
+for i in range(3,n+1):
+    dp[i] = max(dp[i-1], dp[i-2]+ data[i] ,dp[i-3]+data[i-1]+data[i])
 
 
 
-    print(max(dp))
+print(dp[n])
